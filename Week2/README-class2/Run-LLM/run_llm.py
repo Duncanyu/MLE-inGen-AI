66dce388c839ca84297a1cec058f059d9fbe933c
Duncanyu <duncan.w.yu@gmail.com>
2025-07-31 21:41:08 -0400

    Update run_llm.py

--- Week2/README-class2/Run-LLM/run_llm.py
+++ Week2/README-class2/Run-LLM/run_llm.py
@@ -14,3 +14,14 @@ with open("task1_proof.txt", "w") as f:
     f.write(f"Task 1 run at: {datetime.now()}\n")
     f.write(f"Prompt: {prompt}\n")
     f.write(f"Output: {result}\n")
+
+prompt = "Apple is a company that"
+inputs = tokenizer(prompt, return_tensors="pt")
+outputs = model.generate(**inputs, max_new_tokens=10)
+result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(result)
+with open("task2_proof.txt", "w") as f:
+    f.write(f"Task 2 run at: {datetime.now()}\n")
+    f.write(f"Prompt: {prompt}\n")
+    f.write(f"Output: {result}\n")

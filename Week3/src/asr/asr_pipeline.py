dc82bb6d0cc1bbfcfd093c92dd410053f5438220
Duncanyu <duncan.w.yu@gmail.com>
2025-08-08 14:56:21 -0400

    Removed Week3

--- Week3/src/asr/asr_pipeline.py
+++
@@ -1,9 +0,0 @@
-import src.asr.recorder as recorder
-import src.asr.transcriber as transcriber
-
-def get_prompt(model_size = "base", device = "cpu"):
-    audio_path = recorder.record(sample_rate=16000)
-
-    text = transcriber.transcribe(audio_path, model_size=model_size, device=device)
-
-    return text
